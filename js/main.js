4d9f97492c0c15481603931ae692706c44e4e656
Boburkhan <91623890+Boburkhanco@users.noreply.github.com>
2021-11-02 12:26:52 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,53 @@
+
+var studentName = prompt("Ismingiz?");
+console.log(studentName);
+
+var studentSurname = prompt("Familyangiz?");
+console.log(studentSurname);
+
+var studentAge = prompt("Yoshingiz?");
+console.log(studentAge);
+
+var studentHome = prompt("Qayerdansiz?");
+console.log(studentHome);
+
+var studentStudy = prompt("Qayerda o'qiysiz?");
+console.log(studentStudy);
+
+var studentHobby = prompt("Hobbyingiz?");
+console.log(studentHobby);
+
+var studentTravel = prompt("Qaysi davlatga chiqishni xohlaysiz?");
+console.log(studentTravel);
+
+var studentLang = prompt("Nechta tilni bilasiz?");
+console.log(studentLang);
+
+var studentIt = prompt("Sizga qaysi biri ko'proq qiziq: Frontend yoki Backend?");
+console.log(studentIt);
+
+var studentColor = prompt("Sizning yoqtirgan rangingiz?");
+console.log(studentColor);
+
+var studentBrand = prompt("Sizning yoqtirgan brendingiz?");
+console.log(studentBrand);
+
+var studentJob = prompt("Kim bo'lmoqchisiz?");
+console.log(studentJob);
+alert( 
+        "Biz aniqlagan ma'lumotlarga qaraganda, sizning" + "\n" +
+        "Ismingiz:" + " " + studentName + "\n" +
+        "Familyangiz:" + " " + studentSurname + "\n" +
+        "Yoshingiz:" + " " + studentAge + "\n" + 
+        "Yashash joyingiz:" + " " + studentHome + "\n" + 
+        "O'qish joyingiz:" + " " + studentStudy + "\n" + 
+        "Hobbyingiz:" + " " + studentHobby + "\n" + 
+        "Orzuyingizdagi davlat:" + " " + studentTravel + "\n" +
+        "Biladigan tillaringiz soni:" + " " + studentLang + "\n" +
+        "Yoqtirgan dasturlash yo'nalishingiz:" + " " + studentIt + "\n" +
+        "Yoqtirgan rangingiz:" + " " + studentHobby + "\n" + 
+        "Yoqtirgan brandingiz:" + " " + studentHobby + "\n" + 
+        "va sizga kelajakda katta" + " " + studentJob + " " + "bo'lishingizga omad tilaymiz!"  + "\n" +
+
+        "E'tiboringiz uchun rahmat!"
+ );
